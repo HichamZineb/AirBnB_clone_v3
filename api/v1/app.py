691737f80d15b5b145118ccac6fbe185ca34c134
Nawaritta <Nor.benhaddou@gmail.com>
2023-10-28 00:07:17 +0000

    blueprint && not found handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,11 +6,19 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close_session(exception):
     """closes a session"""
     storage.colse()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ handler for 404 errors that returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
 
