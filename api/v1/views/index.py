b2ee3b7b439b98b98fc2787d1c04b1a99d051588
Nawaritta <Nor.benhaddou@gmail.com>
2023-10-28 23:55:54 +0000

    task 4 checked locally

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,27 @@ This module displays the status
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """ returns Json status OK """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def all_objects():
+    """retrieves the number of each objects by type"""
+    return jsonify(amenities=storage.count(Amenity),
+                   cities=storage.count(City),
+                   places=storage.count(Place),
+                   reviews=storage.count(Review),
+                   states=storage.count(State),
+                   users=storage.count(User))
