7893680030f9868901fd6f3c2f290e3bb14a6566
Nawaritta <Nor.benhaddou@gmail.com>
2023-11-02 21:57:55 +0000

    add strict slases in city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,8 +9,8 @@ from datetime import datetime
 import uuid
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'])
-@app_views.route('/states/<state_id>/cities/', methods=['GET'])
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
 def cities_by_state(state_id):
     '''Retrieves a list of all City objects'''
     States = storage.all("State").values()
@@ -22,8 +22,8 @@ def cities_by_state(state_id):
     return jsonify(Cities)
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'])
-@app_views.route('/states/<state_id>/cities/', methods=['POST'])
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
 def create_city(state_id):
     '''Creates a City'''
     if not request.get_json():
@@ -42,7 +42,8 @@ def create_city(state_id):
     return jsonify(cities[0]), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'])
+@app_views.route('/cities/<city_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     '''Retrieves a City object'''
     Cities = storage.all("City").values()
@@ -52,7 +53,8 @@ def get_city(city_id):
     return jsonify(city_obj[0])
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'])
+@app_views.route('/cities/<city_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     '''Deletes a City object'''
     Cities = storage.all("City").values()
@@ -67,7 +69,7 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'])
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def updates_city(city_id):
     '''Updates a City object'''
     Cities = storage.all("City").values()
